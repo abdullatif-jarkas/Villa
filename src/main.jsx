9357126b819ec51f4dd8e418c58343b679bc71cc
abdullatif <abdullatifjr002@gmail.com>
2025-04-05 12:15:27 +0300

    changed repo name

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import ScrollToTop from './components/ScrollToTop';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <React.StrictMode>
-  <BrowserRouter basename='task-3-mern'>
+  <BrowserRouter basename='Villa'>
     <ScrollToTop />
     <App />
   </BrowserRouter>
